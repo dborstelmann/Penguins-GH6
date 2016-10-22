f7a1f887117d9792a0460ad5df773a319b9c88e6
Justin Guyton <guyton.justin@gmail.com>
2016-10-22 18:25:23 -0500

    new client url

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^get_applicants/$', read.get_applicants, name='get_applicants'),
     url(r'^mark_reviewed/$', write.mark_reviewed, name='mark_reviewed'),
     url(r'^get_shelters/$', read.get_shelters, name='get_shelters'),
-    url(r'^update_shelter/$', write.update_shelter, name='update_shelters')
+    url(r'^update_shelter/$', write.update_shelter, name='update_shelters'),
+    url(r'^new_client/$', write.new_client, name='new_client')
 
 ]
