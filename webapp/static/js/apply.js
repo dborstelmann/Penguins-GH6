ed9f1a494f4b57854aac703c20f8c012664e0b84
Daniel Borstelmann <dborstelmann@wustl.edu>
2016-10-21 23:07:34 -0500

    application form

--- webapp/static/js/apply.js
+++ webapp/static/js/apply.js
@@ -10,6 +10,12 @@ hk.ApplyView = BB.View.extend({
 
     render: function () {
         this.$el.empty().append(this.template());
+
+        this.$('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 100 // Creates a dropdown of 15 years to control year
+        });
+        hk.materializeShit();
     },
 
     events: {
