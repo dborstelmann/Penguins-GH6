50138b63629f5fb9c3470da8c2ab01656fd9c472
Darius Calliet <dcalliet@topoppssoftware.com>
2016-10-22 15:13:25 -0500

    adding some urls

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 import read, write
 
 urlpatterns = [
-    url(r'^apply/$', write.apply, name='apply')
+    url(r'^apply/$', write.apply, name='apply'),
+    url(r'^get_clients/$', write.search_clients, name='get_clients'),
+    url(r'^get_applicants/$', write.get_applicants, name='get_applicants'),
+    url(r'^mark_reviewed/$', write.mark_reviewed, name='mark_reviewed')
+
 ]
